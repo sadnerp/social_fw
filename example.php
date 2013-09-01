272b16f41c59c7feb5e7deb68ff39c5d1566319b
Vyacheslav <sadnerp@gmail.com>
2013-09-01 23:45:05 +0400

    Update example.php

--- example.php
+++ example.php
@@ -2,17 +2,15 @@
 
 require_once 'vkapi.class.php';
 
-$app = new vkapi();
+$app        = new vkapi();
 $app->proxy = '192.168.0.2:3128';
 
 /**
  * @see https://oauth.vk.com/authorize?client_id=3382849&scope=offline&display=page&response_type=token
  */
-$app->access_token = "user token";
+$app->access_token  = "user token";
+$current_user       = $app->usersGet();
 
 
-$current_user  = $app->usersGet();
 
 var_dump($current_user);
-
-?>
